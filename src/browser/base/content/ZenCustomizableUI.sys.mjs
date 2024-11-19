de81eeb2b295e03afc2b234dc287cd9f9af80192
mr. M <mauro.balades@tutanota.com>
2024-11-19 21:49:26 +0100

    fix: update default placements for toolbar buttons and enhance tab styles with important flags

--- src/browser/base/content/ZenCustomizableUI.sys.mjs
+++ src/browser/base/content/ZenCustomizableUI.sys.mjs
@@ -9,7 +9,7 @@ export var ZenCustomizableUI = new (class {
       'zen-sidebar-top-buttons',
       {
         type: this.TYPE_TOOLBAR,
-        defaultPlacements: ['zen-sidepanel-button'],
+        defaultPlacements: ['preferences-button', 'zen-sidepanel-button'],
         defaultCollapsed: null,
       },
       true
